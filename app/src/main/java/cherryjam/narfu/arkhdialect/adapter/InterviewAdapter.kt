6dec59a6bd830c69bf0a69da79824f2f7d659fac
SaylesMand <daniel.2003qw@gmail.com>
2024-06-10 17:00:00 +0300

    add options_menu

--- app/src/main/java/cherryjam/narfu/arkhdialect/adapter/InterviewAdapter.kt
+++ app/src/main/java/cherryjam/narfu/arkhdialect/adapter/InterviewAdapter.kt
@@ -1,9 +1,12 @@
 package cherryjam.narfu.arkhdialect.adapter
 
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import cherryjam.narfu.arkhdialect.R
 import cherryjam.narfu.arkhdialect.data.Interview
 import cherryjam.narfu.arkhdialect.databinding.ItemInterviewBinding
 
@@ -15,7 +18,35 @@ class InterviewAdapter() :
             notifyDataSetChanged()
         }
 
-    class InterviewViewHolder(val binding: ItemInterviewBinding) : ViewHolder(binding.root)
+    class InterviewViewHolder(val binding: ItemInterviewBinding) : ViewHolder(binding.root) {
+        init {
+            binding.listItemOptions.setOnClickListener {
+                //creating a popup menu
+                val popup: PopupMenu = PopupMenu(it.context, binding.listItemOptions)
+
+                //inflating menu from xml resource
+                popup.inflate(R.menu.options_menu)
+
+                //adding click listener
+                popup.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
+                    override fun onMenuItemClick(item: MenuItem): Boolean {
+                        return when (item.itemId) {
+                            R.id.select ->
+                                true
+
+                            R.id.delete ->
+                                true
+
+                            else -> false
+                        }
+                    }
+                })
+
+                //displaying the popup
+                popup.show()
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InterviewViewHolder {
         val inflater = LayoutInflater.from(parent.context)
