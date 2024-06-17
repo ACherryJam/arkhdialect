4070ef3fb4df9d443d6b24c9c945f078ef4b57b5
Alexander Starikov <acherryjam@gmail.com>
2024-06-17 21:16:55 +0300

    feat: refactor AlertDialogHelper

--- app/src/main/java/cherryjam/narfu/arkhdialect/utils/AlertDialogHelper.kt
+++ app/src/main/java/cherryjam/narfu/arkhdialect/utils/AlertDialogHelper.kt
@@ -1,194 +1,47 @@
 package cherryjam.narfu.arkhdialect.utils
 
-import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
-import android.content.DialogInterface
-import android.os.Looper
-import android.text.TextUtils
-import android.view.View
-import androidx.fragment.app.Fragment
 
 
-class AlertDialogHelper(val context: Context, val callBack: AlertDialogListener) {
-    var alertDialog: AlertDialog? = null
-    var current_activity = context
+class AlertDialogHelper {
+    companion object {
+        /**
+         * Displays constructed AlertDialog
+         *
+         * @param title
+         * @param message
+         * @param positiveText
+         * @param neutralText
+         * @param negativeText
+         * @param positiveCallback Function to call on positive button click
+         * @param neutralCallback Function to call on neutral button click
+         * @param negativeCallback Function to call on negative button click
+         * @param isCancelable
+         */
+        fun showAlertDialog(
+            context: Context,
+            title: String? = null,
+            message: String? = null,
+            positiveText: String? = null,
+            neutralText: String? = null,
+            negativeText: String? = null,
+            positiveCallback: () -> Unit = {},
+            neutralCallback: () -> Unit = {},
+            negativeCallback: () -> Unit = {},
+            isCancelable: Boolean = false
+        ) {
+            val alertDialogBuilder = AlertDialog.Builder(context)
 
-    /**
-     * Displays the AlertDialog with 3 Action buttons
-     *
-     * you can set cancelable property
-     *
-     * @param title
-     * @param message
-     * @param positive
-     * @param negative
-     * @param neutral
-     * @param from
-     * @param isCancelable
-     */
-    /**
-     * Displays the AlertDialog with 3 Action buttons
-     *
-     * cancelable property is false (Default)
-     *
-     * @param title
-     * @param message
-     * @param positive
-     * @param negative
-     * @param neutral
-     * @param from
-     */
-    @JvmOverloads
-    fun showAlertDialog(
-        title: String?,
-        message: String?,
-        positive: String?,
-        negative: String?,
-        neutral: String?,
-        from: Int,
-        isCancelable: Boolean = false
-    ) {
-        val alertDialogBuilder = AlertDialog.Builder(current_activity)
+            title?.let { alertDialogBuilder.setTitle(title) }
+            message?.let { alertDialogBuilder.setMessage(message) }
 
-        if (!TextUtils.isEmpty(title)) alertDialogBuilder.setTitle(title)
-        if (!TextUtils.isEmpty(message)) alertDialogBuilder.setMessage(message)
+            positiveText?.let { alertDialogBuilder.setPositiveButton(positiveText) { _, _ -> positiveCallback() } }
+            neutralText?.let { alertDialogBuilder.setNeutralButton(neutralText) { _, _ -> neutralCallback() } }
+            negativeText?.let { alertDialogBuilder.setNegativeButton(negativeText) { _, _ -> negativeCallback() } }
 
-        if (!TextUtils.isEmpty(positive)) {
-            alertDialogBuilder.setPositiveButton(
-                positive
-            ) { arg0, arg1 ->
-                callBack.onPositiveClick(from)
-                alertDialog!!.dismiss()
-            }
+            alertDialogBuilder.setCancelable(isCancelable)
+            alertDialogBuilder.create().show()
         }
-        if (!TextUtils.isEmpty(neutral)) {
-            alertDialogBuilder.setNeutralButton(
-                neutral
-            ) { arg0, arg1 ->
-                callBack.onNeutralClick(from)
-                alertDialog!!.dismiss()
-            }
-        }
-        if (!TextUtils.isEmpty(negative)) {
-            alertDialogBuilder.setNegativeButton(
-                negative
-            ) { arg0, arg1 ->
-                callBack.onNegativeClick(from)
-                alertDialog!!.dismiss()
-            }
-        } else {
-            try {
-                // Возможно надо переделать (см. код в DeepSeek)
-                Thread {
-                    Looper.prepare()
-                    alertDialog = alertDialogBuilder.create()
-                    alertDialog?.show()
-
-                    val negative_button = alertDialog?.getButton(DialogInterface.BUTTON_NEGATIVE)
-                    negative_button?.visibility = View.GONE
-
-                    Looper.loop()
-                }.start()
-            } catch (e: Exception) {
-                // TODO: handle exception
-                e.printStackTrace()
-            }
-        }
-
-        alertDialogBuilder.setCancelable(isCancelable)
-
-
-        alertDialog = alertDialogBuilder.create()
-        alertDialog?.show()
-    }
-
-
-    /**
-     * Displays the AlertDialog with positive action button only
-     *
-     * you can set cancelable property
-     *
-     * @param title
-     * @param message
-     * @param positive
-     * @param from
-     * @param isCancelable
-     */
-    fun showAlertDialog(
-        title: String?,
-        message: String?,
-        positive: String?,
-        from: Int,
-        isCancelable: Boolean
-    ) {
-        showAlertDialog(title, message, positive, "", "", from, isCancelable)
-    }
-
-    /**
-     * Displays the AlertDialog with positive action button only
-     *
-     * cancelable property is false (Default)
-     *
-     * @param title
-     * @param message
-     * @param positive
-     * @param from
-     */
-    fun showAlertDialog(title: String?, message: String?, positive: String?, from: Int) {
-        showAlertDialog(title, message, positive, "", "", from, false)
-    }
-
-
-    /**
-     *
-     * Displays the AlertDialog with positive & negative buttons
-     *
-     * you can set cancelable property
-     *
-     * @param title
-     * @param message
-     * @param positive
-     * @param negative
-     * @param from
-     * @param isCancelable
-     */
-    fun showAlertDialog(
-        title: String?,
-        message: String?,
-        positive: String?,
-        negative: String?,
-        from: Int,
-        isCancelable: Boolean
-    ) {
-        showAlertDialog(title, message, positive, negative, "", from, isCancelable)
-    }
-
-    /**
-     *
-     * Displays the AlertDialog with positive & negative buttons
-     *
-     * cancelable property is false (Default)
-     *
-     * @param title
-     * @param message
-     * @param positive
-     * @param negative
-     * @param from
-     */
-    fun showAlertDialog(
-        title: String?,
-        message: String?,
-        positive: String?,
-        negative: String?,
-        from: Int
-    ) {
-        showAlertDialog(title, message, positive, negative, "", from, false)
-    }
-
-    interface AlertDialogListener {
-        fun onPositiveClick(from: Int)
-        fun onNegativeClick(from: Int)
-        fun onNeutralClick(from: Int)
     }
 }
